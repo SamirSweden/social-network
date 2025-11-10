23e28fb49c22ae87505e6bbfa8d8d0999e16b80e
ZolotoY Pers <samirazimov488@gmail.com>
2025-11-10 20:30:04 +0500

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,68 @@
+# auth.py
+from datetime import datetime, timedelta
+from typing import Optional
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+# from passlib.context import CryptContext  # Закомментировать или удалить
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import User
+
+SECRET_KEY = "Mky^?Rfj5O+v?J*ac9:MbC?)?C:KeS8A"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+
+def verify_password(plain_password, stored_password): 
+    return plain_password == stored_password
+
+
+
+def get_password_hash(password): 
+    return password
+
+def authenticate_user(db: Session, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user or not verify_password(password, user.hashed_password): 
+        return False
+    return user
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# Обновленная функция get_current_user
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    db = SessionLocal()
+    try:
+        user = db.query(User).filter(User.username == username).first()
+        if user is None:
+            raise credentials_exception
+        return user
+    finally:
+        db.close()
